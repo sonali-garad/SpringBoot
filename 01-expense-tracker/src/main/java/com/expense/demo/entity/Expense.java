ccde56e0fb899c47946d8bce5fb38e755cbfdae6
sonali garad <sonaligarad1234@gmail.com>
2026-09-09 22:33:16 +0530

    entity-1

---
+++ 01-expense-tracker/src/main/java/com/expense/demo/entity/Expense.java
@@ -0,0 +1,61 @@
+package com.expense.demo.entity;
+
+import java.time.LocalDate;
+
+public class Expense {
+    int id;
+    String title;
+    int amount;
+    LocalDate date;
+    String category;
+    String description;
+
+    public int getId()
+    {
+        return  id ;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
